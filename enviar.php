a405b2d179523fb87a1d95ab8bcdb60b22da4382
cdbarroso777 <73627939+cdbarroso777@users.noreply.github.com>
2020-10-28 23:16:07 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -5,9 +5,8 @@ $destino="informacionlibrosyalgomas@gmail.com";
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 $telefono = $_POST["telefono"];
-$dependencia = $_POST["dependencia"];
 $mensaje = $_POST["mensaje"];
-$contenido = "Nombre: " . $nombre "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nDependencia: " . $dependencia "\nMensaje:" .$mensaje;
+$contenido = "Nombre: " . $nombre "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje:" .$mensaje;
 mail($destino,"Solicitud",$contenido);
 header("Location:gracias.html");
 ?>
